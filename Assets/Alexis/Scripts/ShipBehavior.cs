e91d389cb80aaecc0a357d577c067a5ea549ae65
Alexis Serna <7319aserna@gmail.com>
2019-03-13 17:57:04 -0700

    Android Pirate Game: Goal - Work more on button rotation

--- Assets/Alexis/Scripts/ShipBehavior.cs
+++ Assets/Alexis/Scripts/ShipBehavior.cs
@@ -6,13 +6,19 @@ using UnityEngine.UI;
 
 public class ShipBehavior : MonoBehaviour {
 
-    // public bool isButtonEnabled = false;
-    public float speed = 5.0f;
+    private bool isLeftRotationButtonPressed = false;
+    private bool isRightRotationButtonPressed = false;
+
+    public Button increaseSail;
+    public Button decreaseSail;
+    public Button rotateLeft;
+    public Button rotateRight;
+
+    private float speed = 0.0f;
     public float rotateSpeed = 0.5f;
 
-    // Button button;
     Rigidbody rigidbody = new Rigidbody();
-    Vector3 vector3 = new Vector3();
+    static Vector3 vector3 = new Vector3();
 
     // Use this for initialization
     void Start()
@@ -20,25 +26,65 @@ public class ShipBehavior : MonoBehaviour {
         rigidbody = GetComponent<Rigidbody>();
         //This locks the RigidBody so that it does not move or rotate in the X axis.
         rigidbody.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY;
+
+        Button increaseSailButton = increaseSail.GetComponent<Button>();
+        Button decreaseSailButton = decreaseSail.GetComponent<Button>();
+        Button rotateLeftButton = rotateLeft.GetComponent<Button>();
+        Button rotateRightButton = rotateRight.GetComponent<Button>();
+
+        increaseSailButton.onClick.AddListener(go);
+        decreaseSailButton.onClick.AddListener(stop);
+        rotateLeftButton.onClick.AddListener(rotLeft);
+        rotateRightButton.onClick.AddListener(rotRight);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0, Input.GetAxis("Horizontal") * rotateSpeed, 0); // changes the rotation of the player
+        
+        increaseSail.onClick.AddListener(go);
+        increaseSail.onClick.AddListener(stop);
+        rotateLeft.onClick.AddListener(rotLeft);
+        rotateRight.onClick.AddListener(rotRight);
 
-        vector3 = new Vector3(Input.GetAxis("Vertical"), 0.0f,  0.0f);
+        //transform.Rotate(0, Input.GetAxis("Horizontal") * rotateSpeed, 0); // changes the rotation of the player
 
-        transform.Translate(vector3 * speed * Time.deltaTime);
+        //vector3 = new Vector3(Input.GetAxis("Vertical"), 0.0f, 0.0f);
+
+        transform.Translate(vector3 * Time.deltaTime);
+    }
+    
+    public void go()
+    {
+        vector3 = new Vector3(++speed, 0.0f, 0.0f);
     }
 
-    public void rotateLeft()
+    public void noSail()
     {
-        transform.Rotate(0, -rotateSpeed, 0); // changes the rotation of the player
+        vector3 = new Vector3(0.0f, 0.0f, 0.0f);
     }
 
-    public void rotateRight()
+    public void stop()
     {
-        transform.Rotate(0, rotateSpeed, 0); // changes the rotation of the player
+        vector3 = new Vector3(--speed, 0.0f, 0.0f);
+    }
+
+    public void rotLeft()
+    {
+        isLeftRotationButtonPressed = true;
+        if (isLeftRotationButtonPressed)
+        {
+            transform.Rotate(0, -rotateSpeed, 0);
+        }
+        isLeftRotationButtonPressed = false;
+    }
+
+    public void rotRight()
+    {
+        if (isRightRotationButtonPressed)
+        {
+            transform.Rotate(0, rotateSpeed, 0);
+        }
+        isRightRotationButtonPressed = false;
     }
 }
---
+++ Assets/Alexis/Scripts/shopBehavior.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class shopBehavior : MonoBehaviour {
+    public Canvas canvas;
+    public GameObject gameObject;
+    private ShipBehavior shipBehavior;
+
+    private void Start()
+    {
+        canvas.enabled = false;
+        shipBehavior = gameObject.GetComponent<ShipBehavior>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.name == gameObject.name)
+        {
+            canvas.enabled = true;
+            shipBehavior.noSail();
+        }
+    }
+}
