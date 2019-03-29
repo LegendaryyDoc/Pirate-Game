7d8942e9b256ccad4ade2ba237330a48ab618084
Nathan Knebusch <nateknebusch@gmail.com>
2019-03-29 15:50:32 -0700

    added a new way for the ai to follow the player

--- Assets/Nathan/MadeScripts/AI/ShipAi.cs
+++ Assets/Nathan/MadeScripts/AI/ShipAi.cs
@@ -14,6 +14,7 @@ public class ShipAi : MonoBehaviour
     public float speed = .5f;
     public float turnSpeed = 10f;
 
+    private bool rightSide;
     private float PI = 3.14159265359f;
     private float distanceBetween;
     private bool toClose; // if to close then switched the direction
@@ -21,6 +22,12 @@ public class ShipAi : MonoBehaviour
     private float lerpTime = 1f; // max lerp life
     private float currentLerpTime; // how far in the lerp currently
     private Vector3 relativePoint; // used for determining if other object is on the left or right of the ai
+    private int closestTerm = -1;
+
+    public int amountOfTerminals = 12;
+    private int angleDirection;
+    private Vector3[] terminals;
+
 
     /*-----   Check if player is in range   -----*/
 
@@ -42,9 +49,23 @@ public class ShipAi : MonoBehaviour
 
     /*-------------------------------------------*/
     /*-------------------------------------------*/
-	
-	// Update is called once per frame
-	void Update ()
+
+    private void Awake()
+    {
+        terminals = new Vector3[amountOfTerminals];
+        int angle = 0;
+        for (int i = 0; i < terminals.Length; i++)
+        {
+            angle += 360/amountOfTerminals;
+
+            var x = radiusNearestPoint * Mathf.Cos(Mathf.Deg2Rad * angle);
+            var y = radiusNearestPoint * Mathf.Sin(Mathf.Deg2Rad * angle);
+            terminals[i] = new Vector3(x, 3, y);
+        }
+    }
+
+    // Update is called once per frame
+    void Update ()
     {
         if (target == null)
         {
@@ -58,16 +79,15 @@ public class ShipAi : MonoBehaviour
 
         if (movement == true)
         {
-
             var point = GetPoint();
             // rotate to the target
-            float perc = currentLerpTime / lerpTime / turnSpeed; // for the lerp
 
-            var lookPos = target.position + point - transform.position; // gets the offset of the 2 objects
+            var lookPos = point - transform.position; // gets the offset of the 2 objects
             lookPos.y = 0;
             var rotation = Quaternion.LookRotation(lookPos, Vector3.up); // uses the offest and makes it a angle
 
-            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, perc); // rotates the ai
+            //transform.rotation = Quaternion.Slerp(transform.rotation, rotation, perc); // rotates the ai
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, turnSpeed * Time.deltaTime);
 
             distanceBetween = Vector3.Distance(transform.position, target.position); // checks to see how close or far the player is from the ai
             toClose = distanceBetween < toCloseDistance; // checks to see if to close
@@ -83,24 +103,62 @@ public class ShipAi : MonoBehaviour
         relativePoint = transform.InverseTransformPoint(target.position); // checks what side the player ship is on
         if(relativePoint.x > 0)
         {
-            Debug.Log("Right");
+            rightSide = true;
         }
         else if(relativePoint.x < 0)
         {
-            Debug.Log("Left");
+            rightSide = false;
         }
 	}
 
     Vector3 GetPoint()
     {
-        float difX = (transform.position.x - target.position.x);
+        /*float difX = (transform.position.x - target.position.x);
         float difY = (transform.position.z - target.position.z);
 
         float angle = Mathf.Atan2(difY, difX);
 
-        var x = radiusNearestPoint * Mathf.Cos(angle);
-        var y = radiusNearestPoint * Mathf.Sin(angle);
-        return new Vector3(x, 0, y);
+        var x = radiusNearestPoint * Mathf.Cos(angle + angleDirection);
+        var y = radiusNearestPoint * Mathf.Sin(angle + angleDirection);
+        return new Vector3(x, 0, y);   */
+
+        closestTerm = 0;
+        float closestCurrentDist = Vector3.Distance(transform.position, target.position + terminals[closestTerm]);
+        for(int i = 1; i < terminals.Length; i++)
+        {
+            float contenderClosestDist = Vector3.Distance(transform.position, target.position + terminals[i]);
+            
+            if(contenderClosestDist < closestCurrentDist)
+            {
+                closestTerm = i;
+                closestCurrentDist = contenderClosestDist;
+            }
+        }
+
+        if (rightSide == false)
+        {
+            if (closestTerm == 0)
+            {
+                closestTerm = terminals.Length - 1;
+            }
+            else
+            {
+                closestTerm = closestTerm - 1;
+            }
+        }
+        else
+        {
+            if (closestTerm == terminals.Length)
+            {
+                closestTerm = 0;
+            }
+            else
+            {
+                closestTerm = (closestTerm + 1) % terminals.Length;
+            }
+        }
+
+        return target.position + terminals[closestTerm];
     }
 
     private void OnDrawGizmos()
@@ -109,11 +167,18 @@ public class ShipAi : MonoBehaviour
         {
             return;
         }
-
-        var point = GetPoint();
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawSphere(target.position, 1.0f);
 
         Gizmos.color = Color.red;
+        for (int i = 0; i < terminals.Length; i++)
+        {
+            Gizmos.DrawWireSphere(target.position + terminals[i], 1);
+        }
 
-        Gizmos.DrawWireSphere(new Vector3(target.position.x + point.x, 3, target.position.z + point.z), 1);
+        Gizmos.color = Color.green;
+        Gizmos.DrawLine(transform.position, target.position + terminals[closestTerm]);
+
+        //Gizmos.DrawWireSphere(new Vector3(target.position.x + point.x, 3, target.position.z + point.z), 1);
     }
 }
