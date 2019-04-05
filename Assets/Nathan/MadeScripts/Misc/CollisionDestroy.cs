b34f4865dc6fa80a1545d3caf3a4b68da7cbe07d
Nathan Knebusch <nateknebusch@gmail.com>
2019-04-05 09:44:00 -0700

    added a new player ship health

--- Assets/Nathan/MadeScripts/Misc/CollisionDestroy.cs
+++ Assets/Nathan/MadeScripts/Misc/CollisionDestroy.cs
@@ -4,29 +4,29 @@ using UnityEngine;
 
 public class CollisionDestroy : MonoBehaviour
 {
-    public float damage = 10;
+    public float damage = 10; // how much damage a cannonball does
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter(Collider other) // when the cannonball collides with something
     {
-        if(other.tag == "Player")
+        if(other.tag == "Player") // if object colliding with is a player
         {
-            ShipBehavior player = other.GetComponent<ShipBehavior>();
+            ShipBehavior player = other.GetComponent<ShipBehavior>(); // gets the player script
             if(player == null)
             {
                 return;
             }
-            //subtract health by damage
-            // still need to add player health
+            player.shipHealth -= damage; // subtracts damage from the health of the player
+            Debug.Log("PlayerHealth: " + player.shipHealth);
         }
-        else if(other.tag == "AI")
+        else if(other.tag == "AI") // if object colliding with is an ai
         {
-            ShipAi shipAi = other.GetComponent<ShipAi>();
+            ShipAi shipAi = other.GetComponent<ShipAi>(); // grabs the ai script
             if (shipAi == null)
             {
                 return;
             }
-            shipAi.health -= damage;
-            Debug.Log("Health:" + shipAi.health);
+            shipAi.health -= damage; // subtracts damge from the health of the ai
+            Debug.Log("AIHealth: " + shipAi.health);
         }
     }
 
