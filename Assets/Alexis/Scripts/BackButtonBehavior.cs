90e9040af6324d16c20a965f775493e377c62a73
Alexis Serna <7319aserna@gmail.com>
2019-04-10 14:36:40 -0700

    APG: Implemented Title Screen

---
+++ Assets/Alexis/Scripts/BackButtonBehavior.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class BackButtonBehavior : MonoBehaviour
+{
+    private Button backButton;
+
+    void Start()
+    {
+        backButton = this.GetComponent<Button>();
+        backButton.onClick.AddListener(back);
+    }
+
+    void back()
+    {
+        SceneManager.LoadScene("Title Screen");
+    }
+}
